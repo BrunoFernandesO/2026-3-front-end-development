97db187af432cc36c44a8a101c3adb7f539651cd
Bruno André Fernandes Ortega <fernandes.bruno0609@gmail.com>
2026-03-26 10:04:08 -0300

    Activity 04 draft

--- atividade-4--24-03-2026/01 - script.js
+++ atividade-4--24-03-2026/01 - script.js
@@ -1,6 +1,7 @@
 /*Codigo feito por Bruno Ortega - Aluno de ESOFT3S-M-A*/
 const lilGuy = document.getElementById("lilGuy");
 const food = document.getElementById("food");
+const easterEgg = document.getElementById("easterEgg");
 
 const mood = {
     idle: "IdleLilGuy.png",
@@ -15,6 +16,18 @@ let interval = null;
 let time_click = null;
 let time_out = null;
 
+const easterEggImages = [
+    "ferlini1.jpeg",
+    "ferlini2.jpeg",
+    "ferlini3.jpeg",
+    "ferlini4.jpeg",
+    "ferlini5.jpeg",
+    "ferlini6.jpeg",
+    "ferlini7.jpeg"
+];
+
+let easterEggIndex = 0;
+
 function controls(){
     if(interval) clearInterval(interval);
     interval = setInterval( () =>{
@@ -28,7 +41,31 @@ function controls(){
         if(control == 60){
             lilGuy.src = mood.dead;
         }
-    }, 500);
+    }, 1);
+    console.log(control);
 }
 
-controls();
+function feed(){
+    
+    lilGuy.src = mood.eating;
+    control = 0;
+
+    console.log("Comendo");
+
+    if(time_click) clearTimeout(time_click);
+
+    time_click = setTimeout(() => {
+        lilGuy.src = mood.happy;
+
+        if(time_out) clearTimeout(time_out);
+        time_out = setTimeout(() => {
+            lilGuy.src = mood.idle;
+        }, 2000);
+
+    }, 1000);
+}
+
+function cycleEasterEgg(){
+    easterEgg.src = easterEggImages[easterEggIndex];
+    easterEggIndex = (easterEggIndex + 1) % easterEggImages.length;
+}
